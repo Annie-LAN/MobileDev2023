eb21695d2ed0800e125472b59d7fece8f7d6cc46
aditir126 <aditiram2025@u.northwestern.edu>
2023-02-27 00:48:16 -0600

    edited dropdown feature - compilation error to be fixed

--- App.js
+++ App.js
@@ -3,37 +3,52 @@ import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, Text, SafeAreaView, View, FlatList } from 'react-native';
 import Song from './components/song'
 import Dropdown from './components/song'
-import { useFonts } from 'expo-font';
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropdownMenu from './components/dropdownMenu';
 
 
 export default function App() {
   const[song, setSong] = useState([]);
 
-  // ////KEY////
-  //   const [open, setOpen] = useState(false);
-  //   const [value, setValue] = useState(null);
-  //   const [items, setItems] = useState([
-  //     {label: "N/A", value: null},
-  //     {label: 'A', value: '0'}, 
-  //     {label: 'A#', value: '1'}, 
-  //     {label: 'B', value: '2'}, 
-  //     {label: 'C', value: '3'}, 
-  //     {label: 'C#', value: '4'}, 
-  //     {label: 'D', value: '5'}, 
-  //     {label: 'D#', value: '6'}, 
-  //     {label: 'E', value: '7'}, 
-  //     {label: 'F', value: '8'}, 
-  //     {label: 'F#', value: '9'}, 
-  //     {label: 'G', value: '10'}, 
-  //     {label: 'G#', value: '11'}, 
+  const [key, setKey] = useState('');
+  const [mode, setMode] = useState('');
+  const [timeSig, setTimeSig] = useState('');
 
-  //   ]);
+  const keyOptions = [
+    {label: "N/A", value: null},
+    {label: 'A', value: '0'}, 
+    {label: 'A#', value: '1'}, 
+    {label: 'B', value: '2'}, 
+    {label: 'C', value: '3'}, 
+    {label: 'C#', value: '4'}, 
+    {label: 'D', value: '5'}, 
+    {label: 'D#', value: '6'}, 
+    {label: 'E', value: '7'}, 
+    {label: 'F', value: '8'}, 
+    {label: 'F#', value: '9'}, 
+    {label: 'G', value: '10'}, 
+    {label: 'G#', value: '11'}, 
+  ];
 
-    // ////MODE////
+  const modeOptions = [
+    {label: "N/A", value: null},
+    {label:"Major", value: 0}, 
+    {label:"Minor", value: 1},
+  ];
 
-    //   {label:"Major", value: 0}, 
-    //   {label:"Minor", value: 1}, 
+  const timeSigOptions = [
+    {label: "N/A", value: null},
+    {label:"0", value: 0}, 
+    {label:"1", value: 1},
+    {label: "3", value: 2},
+    {label:"4", value: 3}, 
+    {label:"5", value: 4},
+    {label: "7", value: 5}
+
+  ]
+
+  // const handleValueChange = (value) => {
+  //   setSelectedValue(value);
+  // }
 
 
   const getSongs = (filters) => {
@@ -57,22 +72,26 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
 
-    <Dropdown 
-      items= {[{label: "N/A", value: null},
-      {label: 'A', value: '0'}, 
-      {label: 'A#', value: '1'}, 
-      {label: 'B', value: '2'}, 
-      {label: 'C', value: '3'}, 
-      {label: 'C#', value: '4'}, 
-      {label: 'D', value: '5'}, 
-      {label: 'D#', value: '6'}, 
-      {label: 'E', value: '7'}, 
-      {label: 'F', value: '8'}, 
-      {label: 'F#', value: '9'}, 
-      {label: 'G', value: '10'}, 
-      {label: 'G#', value: '11'}]}
-      holder= "Select a Key"
-    />
+      <DropdownMenu
+        label = "Select a Key"
+        options = {keyOptions}
+        selectedValue = {key}
+        onValueChange = {(itemValue, itemIndex) => setKey(null)}
+      /> 
+
+      <DropdownMenu
+        label = "Select a Mode"
+        options = {modeOptions}
+        selectedValue = {mode}
+        onValueChange = {(itemValue, itemIndex) => setMode(null)}
+      /> 
+
+      <DropdownMenu
+        label = "Select a Time Signature"
+        options = {timeSigOptions}
+        selectedValue = {timeSig}
+        onValueChange = {(itemValue, itemIndex) => setTimeSig(null)}
+      /> 
 
       <View>
         <Text onPress={getSongs} style={styles.generate}>Generate</Text>
@@ -91,25 +110,7 @@ export default function App() {
   );
 }
 
-// export default function App(){
-//   const [loaded] = useFonts({
-//     Inria: require('./assets/fonts/InriaSerif-Regular.ttf'),
-//     InriaItalic: require('./assets/fonts/InriaSerif-LightItalic.ttf'),
-//     InriaLight: require('./assets/fonts/InriaSerif-Light.ttf'),
-//     InriaBoldI: require('./assets/fonts/InriaSerif-BoldItalic.ttf'),
-//     InriaBold: require('./assets/fonts/InriaSerif-Bold.ttf'),
-//   });
 
-//   if (!loaded){
-//     return null;
-//   }
-
-//   return(
-//     <View style={{flex:1}}>
-//       <Text style={{ fontFamily: 'Inria', fontSize: 27 }}>Inria</Text>
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/dropdownMenu.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import Picker from 'react-native-picker';
+
+const DropdownMenu = ({ options, selectedValue, onValueChange, label }) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={onValueChange}
+      >
+        {options.map((option, index) => (
+          <Picker.Item key={index} label={option.label} value={option.value} />
+        ))}
+      </Picker>
+    </View>
+  );
+};
+
+export default DropdownMenu;
